a0aca1823fd219e238948543e7f451eeda2b4170
David Harris <davidharriswork@gmail.com>
2019-12-04 11:45:06 +0000

    testing config change

--- jenkins.conf.js
+++ jenkins.conf.js
@@ -2,33 +2,17 @@
 const lodash = require('lodash');
 const commonConfig = require('./common.conf').config;
 
-// /**
-//  * Helper to set common options on capabilities
-//  *
-//  * @param {Array} capabilitiesArray capabilities to be processed
-//  * @returns {Array|*} an updated array with common capabilities added to each entry
-//  */
-// const setupCapabilities = function (capabilitiesArray) {
-//     const common = {
-//     };
-//     return capabilitiesArray.map(cap => lodash.defaultsDeep(cap, common));
-// };
-
 const localConfig = {
     // ============
     // Capabilities
     // ============
     // Maximum instances to run in parallel.  Can be overridden on a per-browser basis by adding maxInstances option under each capability.
-    maxInstances: 2,
-    maxInstancesPerCapability: 1,
-    // capabilities: setupCapabilities([
+    maxInstances: 1,
     capabilities: [
         {
-            browserName: 'chrome'
-        },
-        // {
-        //     browserName: 'firefox'
-        // }
+            browserName: 'chrome',
+            maxInstances: 3
+        }
     ],
 
     // ===================
@@ -36,46 +20,29 @@ const localConfig = {
     // ===================
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
+    // If authentication is required then add a username and password between https:// and the rest of the URL.  Separate username and password with : and replace any spaces with %20.
+
     baseUrl: process.env.SERVICE_URL || 'https://ivy-dev.aws.defra.cloud',
 
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: ['selenium-standalone', 'firefox-profile'],
     services: ['selenium-standalone'],
-
     seleniumLogs: './logs/selenium',
-
-    // firefoxProfile: {
-    //     'network.proxy.type': 1,
-    //     'network.proxy.http': process.env.BROWSER_PROXY_HOST,
-    //     'network.proxy.http_port': process.env.BROWSER_PROXY_PORT ? parseInt(process.env.BROWSER_PROXY_PORT) : 3128,
-    //     'network.proxy.ssl': process.env.BROWSER_PROXY_HOST,
-    //     'network.proxy.ssl_port': process.env.BROWSER_PROXY_PORT ? parseInt(process.env.BROWSER_PROXY_PORT) : 3128,
-    //
-    //     // Workaround for issue with firefox when multiple aliases for localhost are defined in the hosts file
-    //     // Without this, the Firefox driver will respond with BAD REQUEST to all calls.
-    //     'webdriver_firefox_allowed_hosts': 'localhost,ip6-localhost,ip6-loopback'
-    // },
-    // seleniumArgs: {
-    //     version: '3.4.0'
-    // },
-    // seleniumInstallArgs: {
-    //     version: '3.4.0'
-    // }
-    // seleniumArgs: {
-    //     drivers: {
-    //         chrome: { version: '78.0.3904.105' },
-    //         // firefox: { version: '0.25.0' },
-    //     }
-    // }, // latest is 3.8.1 but this can cause issues
-    // seleniumInstallArgs: {
-    //     drivers: {
-    //         chrome: { version: '78.0.3904.105' },
-    //         // firefox: { version: '0.25.0' },
-    //     }
-    // }
-
+    seleniumInstallArgs: {
+        drivers: {
+            chrome: {
+                version: '77.0.3865.40',
+            }
+        }
+    },
+    seleniumArgs: {
+        drivers: {
+            chrome: {
+                version: '77.0.3865.40',
+            }
+        }
+    },
 };
 exports.config = lodash.defaultsDeep(localConfig, commonConfig);
